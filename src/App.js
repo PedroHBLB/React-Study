c5f27fd5ca3dcd38731a68e824398220b68fc86c
Pedro Barbieri <85046306+PedroHBLB@users.noreply.github.com>
2022-12-19 18:33:11 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    state = {
-      name: 'Henrique',
-      contador: 0
-    };
+class App extends React.Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      }
+    ]
+  };
 
   render() {
-    const { name } = this.state;
-    const { contador } = this.state;
-
-    const handlePClick = () => {
-      this.setState({name: 'Pedro'});
-    }
-
-    const handleAClick = (event) => {
-      event.preventDefault();
-      this.setState({contador: contador + 1});
-    }
+    const { posts } = this.state
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={handlePClick}>
-            {name} {contador}
-          </p>
-          <a
-            onClick={handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
