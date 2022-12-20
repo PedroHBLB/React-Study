49e4c2e6c57729e3233644bf4e1b54ea223e8d80
Pedro Barbieri <85046306+PedroHBLB@users.noreply.github.com>
2022-12-19 22:46:47 -0300

    Lifecycle methods

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -21,12 +22,40 @@ class App extends React.Component {
       }
     ]
   };
+  timeOutUpdate = null;
+
+  // Lifecycle methods
+  
+  // É invocado imediatamente após um componente ser montado
+  componentDidMount () {
+    this.handleTimeout();
+  }
+
+  // É invocado imediatamente após alguma atualização ocorrer
+  componentDidUpdate () {
+    this.handleTimeout();
+  }
+
+  // É invocado imediatamente antes que um componente seja desmontado e destruído
+  // qualquer limpeza necessária deve ser realizada neste método
+  componenWillUnmount (){
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state
+    posts[0].title = 'O título mudou';
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 1000)
+  }
 
   render() {
-    const { posts } = this.state
+    const { posts, counter } = this.state
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
