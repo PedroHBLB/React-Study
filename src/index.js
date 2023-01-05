1b20dbfcde1c03ba2e6fc580903b554b6cdc1dfe
Pedro Henrique Barbosa Leite Barbieri <85046306+PedroHBLB@users.noreply.github.com>
2023-01-05 13:38:56 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global-styles.css';
-
-import { Home } from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <></>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
